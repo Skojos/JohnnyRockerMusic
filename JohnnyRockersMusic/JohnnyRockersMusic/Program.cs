4ab27a5836f833ef018699ed3b2d88b37cf451cf
Skojos <62700429+Skojos@users.noreply.github.com>
2021-05-14 10:17:42 +0200

    Main
    Final Edit

--- JohnnyRockersMusic/JohnnyRockersMusic/Program.cs
+++ JohnnyRockersMusic/JohnnyRockersMusic/Program.cs
@@ -8,10 +8,6 @@ namespace JohnnyRockersMusic
         {
 
 
-
-
-
-           
             Item cdOne = new CompactDisc("Punisher", "Phoebe Bridgers", 2020, 10, "CD", 200.0);
             Item lpOne = new LongPlay("What kinda music", "Tome Mish", 2020, 10, "LP", 150.0);
             Item lpTwo = new LongPlay("Little Oblivious", "Julien Barker", 2021, 10, "LP", 120.0);
@@ -19,25 +15,12 @@ namespace JohnnyRockersMusic
 
 
 
-
-            /*  Order[] items = new Order[2];
-
-
-
-              for (int i = 0; i < items.Length; i++)
-              {
-                  items[i] = new Order(bookOne, bookTwo);
-              }
-
-              */
-
-
             Item[] order = { cdOne, lpOne, lpTwo };
             Order order1 = new Order(373928,order);
             order1.GetReceipt();
 
             Item[] orderNew = { lpOne, lpTwo };
-            Order order2 = new Order(372928, orderNew);
+            Order order2 = new Order(372929, orderNew);
             order2.GetReceipt();
 
 
