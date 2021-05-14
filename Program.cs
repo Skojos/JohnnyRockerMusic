3a03266348fa5bd4d4a326b48d6e864983209166
Skojos <62700429+Skojos@users.noreply.github.com>
2021-05-14 10:17:12 +0200

    Edit
    Final edit on main

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace JohnnyRockersMusic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Item cdOne = new CompactDisc("Punisher", "Phoebe Bridgers", 2020, 10, "CD", 200.0);
+            Item lpOne = new LongPlay("What kinda music", "Tome Mish", 2020, 10, "LP", 150.0);
+            Item lpTwo = new LongPlay("Little Oblivious", "Julien Barker", 2021, 10, "LP", 120.0);
+
+
+
+
+            Item[] order = { cdOne, lpOne, lpTwo };
+            Order order1 = new Order(373928,order);
+            order1.GetReceipt();
+
+            Item[] orderNew = { lpOne, lpTwo };
+            Order order2 = new Order(372929, orderNew);
+            order2.GetReceipt();
+
+
+
+
+        }
+    }
+}
